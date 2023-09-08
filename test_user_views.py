6dc303d7d41171f5e2d0fe3b119a202ddce39203
Brianna Tran <briannatran45@gmail.com>
2023-09-08 11:52:17 -0700

    added routes to test

--- test_user_views.py
+++ test_user_views.py
@@ -94,24 +94,140 @@ class UserViewTestCase(TestCase):
                                      'image_url': '',
                                      'header_image_url': '',
                                      'location': 'LA'},
-                                     follow_redirects=True)
+                               follow_redirects=True)
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("<!-- Test for /users route.", html)
 
+    def test_user_login_success(self):
+        """Tests if user is able to login with valid credentials"""
+        with self.client as client:
+            resp = client.post('/login',
+                               data={'username': 'u1',
+                                     'password': 'password'},
+                               follow_redirects=True)
+            html = resp.get_data(as_text=True)
 
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Hello, u1!", html)
 
+    def test_user_login_fail(self):
+        """Tests if login fails with incorrect credentials"""
+        with self.client as client:
+            resp = client.post('/login',
+                               data={'username': 'u1',
+                                     'password': 'password1'})
+            html = resp.get_data(as_text=True)
 
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Invalid credentials", html)
 
-    # def test_show_users_list(self):
-    #     """Tests if user listing page is loaded correctly"""
-    #     with self.client as client:
-    #         user = User.query.get(self.u1_id)
-    #         do_login(user)
+    def test_logout(self):
+        """Tests for logout if user is logged in"""
+        with self.client as client:
+            resp = client.post('/login',
+                               data={'username': 'u1',
+                                     'password': 'password'})
 
-    #         resp = client.get('/users')
-    #         html = resp.get_data(as_text=True)
+            logout_resp = client.post('/logout',
+                                      follow_redirects=True)
+            html = logout_resp.get_data(as_text=True)
 
-    #         self.assertEqual(resp.status_code, 200)
-    #         self.assertIn('<!-- Test for /users route.', html)
+            self.assertEqual(logout_resp.status_code, 200)
+            self.assertIn("Welcome back.", html)
+
+    def test_logout_fail(self):
+        """Tests for failed logout if no user is logged in"""
+        with self.client as client:
+            logout_resp = client.post('/logout',
+                                      follow_redirects=True)
+            html = logout_resp.get_data(as_text=True)
+
+            self.assertEqual(logout_resp.status_code, 200)
+            self.assertIn("Access unauthorized", html)
+
+    def test_show_users_list(self):
+        """Tests if user listing page is loaded correctly"""
+        with self.client as client:
+            resp_login = client.post('/login',
+                                     data={'username': 'u1',
+                                           'password': 'password'})
+
+            resp_search = client.get('/users')
+            html = resp_search.get_data(as_text=True)
+
+            self.assertEqual(resp_search.status_code, 200)
+            self.assertIn('<!-- tests for search user', html)
+
+    def test_show_user_profile(self):
+        """Tests if user profile is being displayed"""
+        with self.client as client:
+            resp_login = client.post('/login',
+                                     data={'username': 'u1',
+                                           'password': 'password'})
+
+            resp = client.get(f'/users/{self.u1_id}')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<!-- tests for user profile', html)
+
+    def test_user_following_success(self):
+        """Tests if user following is displayed when logged in"""
+        with self.client as client:
+            resp_login = client.post('/login',
+                                     data={'username': 'u1',
+                                           'password': 'password'})
+
+            resp = client.get(f'/users/{self.u1_id}/following')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<!-- test for following', html)
+
+    def test_user_following_fail(self):
+        """Tests if user following fails to show when not logged in"""
+        with self.client as client:
+            resp = client.get(f'/users/{self.u1_id}/following',
+                              follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Access unauthorized', html)
+
+    def test_user_follower(self):
+        """Tests if user followers is displayed when logged in"""
+        with self.client as client:
+            resp_login = client.post('/login',
+                                     data={'username': 'u1',
+                                           'password': 'password'})
+
+            resp = client.get(f'/users/{self.u1_id}/followers')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<!-- test for followers', html)
+
+    def test_show_likes_success(self):
+        """Tests showing likes for user"""
+        with self.client as client:
+            resp_login = client.post('/login',
+                                     data={'username': 'u1',
+                                           'password': 'password'})
+
+            resp = client.get(f'/users/{self.u1_id}/likes')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<!-- test for showing likes', html)
+
+    def test_show_likes_fail(self):
+        """Tests for likes page if user is not logged in. Should result in authorization error"""
+        with self.client as client:
+            resp = client.get(f'/users/{self.u1_id}/likes',
+                              follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Access unauthorized', html)
